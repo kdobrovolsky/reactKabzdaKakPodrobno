2ffd7aaba95cc6ce176c01692bc69d3b8759ece7
kdobrovolsky <kdobrovolsky02@gmail.com>
2025-06-14 16:21:54 +0300

    Conditional rendering

--- src/components/accordion/Accordion.tsx
+++ src/components/accordion/Accordion.tsx
@@ -7,15 +7,10 @@ type  AccordionPropsType = {
 }
 
 export const Accordion = ({title,collapsed}:AccordionPropsType) => {
-  if(collapsed){
+  
     return  <div>
     <AccordionTitle title={title}/>
-    <AccordionBody />
+    {!collapsed && <AccordionBody />}
   </div>
-  } else{
-    return  <div>
-    <AccordionTitle title={title}/>
-  </div>
-  }
   
   };
